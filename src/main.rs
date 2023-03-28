34b4f25a55c0cb2bf88b30480d32ccb913f92a90
Jacob Highfield <camperboy1000@gmail.com>
2023-03-28 17:29:36 -0400

    Add docs and lots of error handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, io};
+use std::{env, process};
 
 use actix_web::{web, App, HttpServer};
 use log::{error, info, LevelFilter};
 use sqlx::PgPool;
 
+static APP_NAME: &str = "Laundry-API";
+
+/// Initialize the logging system, using [syslog] as the backend.
 fn initalize_syslog() {
     let log_level: LevelFilter = match env::var("LOG_LEVEL") {
         Err(_) => log::LevelFilter::Warn,
@@ -17,45 +20,68 @@ fn initalize_syslog() {
             _ => log::LevelFilter::Warn,
         },
     };
-    let log_result = syslog::init(syslog::Facility::LOG_SYSLOG, log_level, None);
-    if log_result.is_err() {
-        eprintln!("WARNING! Failed to initialize logging system! Server logs will be unavaliable!");
+
+    if syslog::init(syslog::Facility::LOG_SYSLOG, log_level, Some(APP_NAME)).is_err() {
+        eprintln!("WARNING: Failed to initialize logging system! Server logs will be unavaliable!");
     }
 }
 
+/// Parses and returns the database URL from the DATABASE_URL [environment variable](std::env::var).
+///
+/// # Exits
+/// The DATABASE_URL environment variable  not being set is considered an unrecoverable error and exits the process.
 fn parse_database_url() -> String {
     match env::var("DATABASE_URL") {
         Ok(url) => url,
         Err(err) => {
-            let message = format!("ERROR: Unable to parse DATABASE_URL enviroment variable: {err}");
-            error!("{message}");
-            eprintln!("{message}");
-            panic!("{err}");
+            throw_error_message(format!(
+                "Unable to parse DATABASE_URL enviroment variable: {err}"
+            ));
+            process::exit(1);
         }
     }
 }
 
+/// This function attempts to send an error message to the logger and also prints the message to STDERR.
+///
+/// The message is a [String] which is passed to the system log and STDERR.
+fn throw_error_message(message: String) {
+    error!("ERROR: {message}");
+    eprintln!("ERROR: {message}\n");
+}
+
 #[actix_web::main]
-async fn main() -> io::Result<()> {
+async fn main() {
     initalize_syslog();
 
     let database_url = parse_database_url();
 
     let pool = match PgPool::connect_lazy(database_url.as_str()) {
         Ok(pool) => {
-            info!("Connected to the database");
+            info!("INFO: Connected to the database");
             pool
         }
         Err(err) => {
-            let message = format!("ERROR: Failed to connect to the database: {err}");
-            error!("{message}");
-            eprintln!("{message}");
-            panic!("{err}");
+            throw_error_message(format!("Failed to connect to the database: {err}"));
+            process::exit(1);
         }
     };
 
-    HttpServer::new(|| App::new().service(web::scope("/user")))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let http_server = HttpServer::new(|| App::new().service(web::scope("/user")));
+
+    let http_server = match http_server.bind(("127.0.0.1", 8080)) {
+        Ok(server) => server,
+        Err(err) => {
+            throw_error_message(format!("Failed to bind the webserver: {err}"));
+            process::exit(1);
+        }
+    };
+
+    match http_server.run().await {
+        Ok(_) => (),
+        Err(err) => {
+            throw_error_message(format!("Gave up waiting for HttpServer to run: {err}"));
+            process::exit(1);
+        }
+    };
 }
