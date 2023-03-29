831cabb297201a65dd8029bfeced580359866332
Jacob Highfield <camperboy1000@gmail.com>
2023-03-29 01:16:25 -0400

    Cleanup our own logging

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,13 @@ use std::{env, process};
 
 use actix_web::{web, App, HttpServer};
 use laundry_api::{machine, models::AppState, room};
-use log::{error, info, LevelFilter};
 use sqlx::{PgPool, Pool, Postgres};
 
-static APP_NAME: &str = "Laundry-API";
+const APP_NAME: &str = "Laundry-API";
 
 /// Initialize the logging system, using [syslog] as the backend.
 fn initalize_syslog() {
-    let log_level: LevelFilter = match env::var("LOG_LEVEL") {
+    let log_level = match env::var("LOG_LEVEL") {
         Err(_) => log::LevelFilter::Warn,
         Ok(value) => match value.to_uppercase().as_str() {
             "ERROR" => log::LevelFilter::Error,
@@ -37,31 +36,18 @@ fn connect_postgres_database() -> Pool<Postgres> {
     let database_url = match env::var("DATABASE_URL") {
         Ok(url) => url,
         Err(err) => {
-            throw_error_message(format!(
-                "Unable to parse DATABASE_URL enviroment variable: {err}"
-            ));
+            eprintln!("Unable to parse DATABASE_URL enviroment variable: {err}");
             process::exit(1);
         }
     };
 
     match PgPool::connect_lazy(database_url.as_str()) {
-        Ok(pool) => {
-            info!("Connected to the database");
-            pool
-        }
+        Ok(pool) => pool,
         Err(err) => {
-            throw_error_message(format!("Failed to connect to the database: {err}"));
+            eprintln!("Failed to connect to the database: {err}");
             process::exit(1);
         }
     }
-}
-
-/// This function attempts to send an error message to the logger and also prints the message to STDERR.
-///
-/// The message is a [String] which is passed to the system log and STDERR.
-fn throw_error_message(message: String) {
-    error!("{message}");
-    eprintln!("ERROR: {message}\n");
 }
 
 #[actix_web::main]
@@ -89,7 +75,7 @@ async fn main() {
     let http_server = match http_server.bind(("127.0.0.1", 8080)) {
         Ok(server) => server,
         Err(err) => {
-            throw_error_message(format!("Failed to bind the webserver: {err}"));
+            eprintln!("ERROR! Failed to bind the webserver: {err}");
             process::exit(1);
         }
     };
@@ -97,7 +83,7 @@ async fn main() {
     match http_server.run().await {
         Ok(_) => (),
         Err(err) => {
-            throw_error_message(format!("Gave up waiting for HttpServer to run: {err}"));
+            eprintln!("ERROR! Gave up waiting for HttpServer to run: {err}");
             process::exit(1);
         }
     };
