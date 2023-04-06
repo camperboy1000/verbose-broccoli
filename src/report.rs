bd6c29b3de269b8471f9cc6e84c8c5818a078f01
Jacob Highfield <camperboy1000@gmail.com>
2023-04-06 15:29:01 -0400

    Implement deleting a row

--- src/report.rs
+++ src/report.rs
@@ -1,11 +1,11 @@
 use actix_web::{
-    get, post,
+    delete, get, post,
     web::{Data, Json, Path},
     HttpResponse, Responder,
 };
 use chrono::Utc;
 use serde::{Deserialize, Serialize};
-use sqlx::{query, query_as};
+use sqlx::{query, query_as, Pool, Postgres};
 
 use crate::models::{AppState, Report, ReportType};
 
@@ -16,6 +16,49 @@ struct ReportSubmission {
     reporter_username: String,
     report_type: ReportType,
     description: Option<String>,
+}
+
+async fn is_report_present(
+    database: &Pool<Postgres>,
+    report_id: &i32,
+) -> Result<bool, sqlx::Error> {
+    match query!(
+        r#"
+        SELECT id
+        FROM report
+        WHERE id = $1
+        "#,
+        report_id
+    )
+    .fetch_optional(database)
+    .await
+    {
+        Ok(present) => Ok(present.is_some()),
+        Err(err) => Err(err),
+    }
+}
+
+async fn is_machine_present(
+    database: &Pool<Postgres>,
+    machine_id: &String,
+    room_id: &i32,
+) -> Result<bool, sqlx::Error> {
+    match query!(
+        r#"
+        SELECT machine_id, room_id
+        FROM report
+        WHERE machine_id = $1
+        AND room_id = $2
+        "#,
+        machine_id,
+        room_id
+    )
+    .fetch_optional(database)
+    .await
+    {
+        Ok(present) => Ok(present.is_some()),
+        Err(err) => Err(err),
+    }
 }
 
 #[get("/")]
@@ -75,31 +118,25 @@ async fn get_report(data: Data<AppState>, path: Path<i32>) -> impl Responder {
     }
 }
 
-#[post("/submit")]
+#[post("/")]
 async fn submit_report(
     data: Data<AppState>,
     Json(report_submission): Json<ReportSubmission>,
 ) -> impl Responder {
-    let machine_id_present = match query!(
-        r#"
-        SELECT machine_id, room_id
-        FROM report
-        WHERE machine_id = $1
-        AND room_id = $2
-        "#,
+    let machine_present = match is_machine_present(
+        &data.database,
         &report_submission.machine_id,
-        &report_submission.room_id
+        &report_submission.room_id,
     )
-    .fetch_optional(&data.database)
     .await
     {
-        Ok(machine_id) => machine_id.is_some(),
+        Ok(machine_present) => machine_present,
         Err(err) => return HttpResponse::InternalServerError().body(err.to_string()),
     };
 
-    if machine_id_present {
+    if machine_present {
         return HttpResponse::BadRequest().body(format!(
-            "room_id: {} machine_id: {} does not map to a valid machine",
+            "room_id: {} machine_id: {} does not map to a valid machine.",
             &report_submission.room_id, &report_submission.machine_id
         ));
     }
@@ -133,5 +170,43 @@ async fn submit_report(
             sqlx::Error::Database(err) => HttpResponse::BadRequest().body(err.to_string()),
             _ => HttpResponse::InternalServerError().body(err.to_string()),
         },
+    }
+}
+
+#[delete("/{report_id}")]
+async fn delete_report(data: Data<AppState>, path: Path<i32>) -> impl Responder {
+    let report_id = path.into_inner();
+
+    let report_present = match is_report_present(&data.database, &report_id).await {
+        Ok(present) => present,
+        Err(err) => return HttpResponse::InternalServerError().body(err.to_string()),
+    };
+
+    if !report_present {
+        return HttpResponse::NotFound().body(format!("The report {report_id} does not exist."));
+    }
+
+    match query_as!(
+        Report,
+        r#"
+    DELETE FROM report
+    WHERE id = $1
+    RETURNING
+        id as "report_id: i32",
+        machine_id,
+        room_id,
+        reporter_username,
+        time,
+        type as "report_type: ReportType",
+        description,
+        archived
+    "#,
+        report_id
+    )
+    .fetch_one(&data.database)
+    .await
+    {
+        Ok(report) => HttpResponse::Ok().json(report),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
     }
 }
